39b0b3c80e0567728c1db15854c3547f6c9e57b5
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-13 18:01:22 -0700

    modifyLine() function
    modifyLine() function that fixes those three failing tests. Just some simple regex pattern matching.
    Now this will correctly account for negatives, and so far seems to work with any line with numerics at the end (we don't care about non-numerics, because this function is only ever passed lines that end in numerics)

--- demo/src/main/java/x10/x10script.java
+++ demo/src/main/java/x10/x10script.java
@@ -1,7 +1,22 @@
 package x10;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class x10script {
     public static void main(String[] args) {
         System.out.println("Starting program.");
+    public static String modifyLine(String currLine) {
+        String output = "";
+        // grab just the numeric portion at the end (preserving the sign)
+        String pattern = "(-?\\d+(?:\\.\\d+)?)";
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(currLine);
+        if (matcher.find()) {
+            String numericalValueStr = matcher.group();
+            double numericalValue = Double.parseDouble(numericalValueStr);
+            double multipliedValue = numericalValue * 10;
+            output = currLine.replace(numericalValueStr, String.valueOf(multipliedValue));
+        }
+        return output;
     }
 }

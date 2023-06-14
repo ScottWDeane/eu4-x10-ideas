24637d623834cbea1d92e51a0b543bdf259420ad
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-13 18:51:31 -0700

    Update x10script.java
    more potentially useful imports

--- demo/src/main/java/x10/x10script.java
+++ demo/src/main/java/x10/x10script.java
@@ -1,8 +1,12 @@
 package x10;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.io.FileUtils;

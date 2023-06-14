31a8127c609214cad234ce7f91c20d21cccfa35f
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-13 17:49:37 -0700

    initial three tests
    Not quite TDD, but I wrote three tests that I want all to pass at once regarding the "modifyLine()" function. I already have the regex expression available, so I just need to make sure my business logic is sound (mostly the extraction, multiplication, and in-place replacement)

---
+++ demo/src/test/java/x10/x10scriptTest.java
@@ -0,0 +1,37 @@
+package x10;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class x10scriptTest {
+    // I hate writing tests for file/directory manipulation in a windows environment (gross), so I'm
+    // just... not going to.
+
+    @Test
+    public void whenGiven_lineEndingIn0Point1_return_1Point0() {
+        // strings ending in numerics should be multiplied by 10
+        String lineToModify = "nobles_loyalty_modifier = 0.1";
+        String expectedOutput = "nobles_loyalty_modifier = 1.0";
+        String modifiedLine = x10script.modifyLine(lineToModify);
+        Assertions.assertEquals(expectedOutput, modifiedLine);
+    }
+
+    @Test
+    public void whenGiven_lineEndingIn1_return_10() {
+        // strings ending in numerics should be multiplied by 10
+        String lineToModify = "leader_siege = 1";
+        String expectedOutput = "leader_siege = 10.0";
+        String modifiedLine = x10script.modifyLine(lineToModify);
+        Assertions.assertEquals(expectedOutput, modifiedLine);
+    }
+
+    @Test
+    public void whenGiven_lineEndingInNegative0Point05_return_Negative0PointFive() {
+        // strings ending in numerics should be multiplied by 10
+        String lineToModify = "development_cost = -0.05";
+        String expectedOutput = "development_cost = -0.5";
+        String modifiedLine = x10script.modifyLine(lineToModify);
+        Assertions.assertEquals(expectedOutput, modifiedLine);
+    }
+
+}

f8fc49a3852e271a4262c7778bd6a19adb087487
Scott Wayne Deane <scottwdeane@gmail.com>
2023-06-13 18:49:12 -0700

    add function for deleting all old Output files
    add a few imports

--- demo/src/main/java/x10/x10script.java
+++ demo/src/main/java/x10/x10script.java
@@ -1,10 +1,29 @@
 package x10;
 
+import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 public class x10script {
     public static void main(String[] args) {
         System.out.println("Starting program.");
+
+        // clear out our output folder
+        deleteAllOldOutputFiles("./demo/src/output");
+
+    public static void deleteAllOldOutputFiles(String outputFolder) {
+        File[] files = new File(outputFolder).listFiles();
+        if (files.length > 0) {
+            for (File oldOutputFile : files) {
+                oldOutputFile.delete();
+            }
+        }
+    }
+
+
+
+
     public static String modifyLine(String input) {
         Pattern pattern = Pattern.compile("[-+]?\\d*\\.?\\d+");
         Matcher matcher = pattern.matcher(input);
